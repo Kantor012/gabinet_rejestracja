2ca4c53ebe8c0663b1a569c79a3e45ce92023f26
Kantor012 <dkantorczyk@gmail.com>
2023-01-03 22:37:59 +0100

    .

---
+++ Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace gabinet_rejestracja.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: User/Register
+        [HttpPost]
+        public ActionResult Register(UserModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // W tym miejscu dodajemy kod do rejestracji użytkownika
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
