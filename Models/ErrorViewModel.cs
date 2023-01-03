902292af2707c793d8dbbef823daeb01f0cdc077
Kantor012 <dkantorczyk@gmail.com>
2023-01-03 19:28:20 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gabinet_rejestracja.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
