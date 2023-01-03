f17963e0c97829a2a5bb5e1aec0b66681815b540
Kantor012 <dkantorczyk@gmail.com>
2023-01-03 20:24:17 +0100

    .

---
+++ Models/Doctor.cs
@@ -0,0 +1,14 @@
+﻿namespace gabinet_rejestracja.Models
+{
+    public class Doctor
+    {
+        public Doctor()
+        {
+        }
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
