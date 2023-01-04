f574626c082ec39be6de32473b30c48fd256a3e4
Kantor012 <dkantorczyk@gmail.com>
2023-01-04 16:27:03 +0100

    .

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data.Entity;
+using System.Configuration;
+using gabinet_rejestracja.Models;
+using Microsoft.AspNetCore.Mvc;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
+using System.Data.SqlClient;
 
 namespace gabinet_rejestracja.Controllers
 {
@@ -10,6 +15,11 @@ namespace gabinet_rejestracja.Controllers
             return View();
         }
 
+        public class ApplicationDbContext : DbContext
+        {
+            public DbSet<UserModel> Users { get; set; } 
+        }
+
         // POST: User/Register
         [HttpPost]
         public ActionResult Register(UserModel model)
@@ -18,9 +28,29 @@ namespace gabinet_rejestracja.Controllers
             {
                 return View(model);
             }
-
-            // W tym miejscu dodajemy kod do rejestracji użytkownika
-
+            else
+            {
+                //var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                using (var db = new SqlConnection("Data Source=servergabinet.database.windows.net;Initial Catalog=gabinetbaza;User ID=adming;Password=Qwerty231;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+                {
+                    var user = new UserModel
+                    {
+                        Email = model.Email,
+                        Password = model.Password,
+                        ConfirmPassword = model.ConfirmPassword,
+                    };
+                    db.Open();
+                    string sql = "INSERT INTO [dbo].[Users] ([UserId], [Email], [Password], [ConfirmPassword]) VALUES (ABS(CHECKSUM(NEWID()) % 2147483647) + 1, @Email, @Password, @ConfirmPassword)";
+                    var command = new SqlCommand(sql, db);
+                    command.Parameters.AddWithValue("@Email", user.Email);
+                    command.Parameters.AddWithValue("@Password", user.Password);
+                    command.Parameters.AddWithValue("@ConfirmPassword", user.ConfirmPassword);
+                    
+                    command.ExecuteNonQuery();
+                    //db.Users.Add(user);
+                    //db.SaveChanges();
+                }
+            }
             return RedirectToAction("Index", "Home");
         }
     }
